97fed799ae5b765970e90e14a57d6c72753744a3
Matthias Mohr <webmaster@mamo-net.de>
2023-07-03 22:39:40 +0200

    Create/Add geoparquet files

---
+++ .github/workflows/geoparquet.sh
@@ -0,0 +1,7 @@
+stac geoparquet-items create disasters/ disasters/stac-items.geoparquet --collection disasters/collection.json
+stac geoparquet-items create education/bloom/ education/bloom/stac-items.geoparquet --collection education/bloom/collection.json
+stac geoparquet-items create fusion/14N/29E-188N/ fusion/14N/29E-188N/stac-items.geoparquet --collection fusion/14N/29E-188N/collection.json
+stac geoparquet-items create open-skysat-data/ open-skysat-data/stac-items.geoparquet --collection open-skysat-data/collection.json
+stac geoparquet-items create spacenet7/sn7_test_source/ spacenet7/sn7_test_source/stac-items.geoparquet --collection spacenet7/sn7_test_source/collection.json
+stac geoparquet-items create spacenet7/sn7_train_labels/ spacenet7/sn7_train_labels/stac-items.geoparquet --collection spacenet7/sn7_train_labels/collection.json
+stac geoparquet-items create spacenet7/sn7_train_source/ spacenet7/sn7_train_source/stac-items.geoparquet --collection spacenet7/sn7_train_source/collection.json
